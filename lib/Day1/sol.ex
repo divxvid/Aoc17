dc6614fc6968fc4a75d3c04d68a48099a7e9ccd7
divxvid <divyansh.shrivastava@students.iiit.ac.in>
2024-04-14 20:45:23 +0530

    Solve Day1 Part2

--- lib/Day1/sol.ex
+++ lib/Day1/sol.ex
@@ -17,6 +17,18 @@ defmodule Aoc17.Day1 do
 
   @spec part2(String.t()) :: integer
   def part2(input) do
-    input
+    input = String.trim(input)
+    target_length = String.length(input) |> div(2)
+
+    {left, _} = String.split_at(input, target_length)
+    input = (input <> left) |> String.to_charlist()
+
+    Enum.zip(input, Enum.drop(input, target_length))
+    |> Enum.reduce(0, fn {last, curr}, acc ->
+      cond do
+        last == curr -> acc + (last - ?0)
+        true -> acc
+      end
+    end)
   end
 end
--- test/Day1/day1_test.exs
+++ test/Day1/day1_test.exs
@@ -23,4 +23,28 @@ defmodule Aoc17.Day1Test do
     expected = 1223
     assert expected == got
   end
+
+  test "testing samples for part2" do
+    test_data = %{
+      "1212" => 6,
+      "1221" => 0,
+      "123425" => 4,
+      "123123" => 12,
+      "12131415" => 4
+    }
+
+    Enum.each(test_data, fn {input, expected} ->
+      got = Aoc17.Day1.part2(input)
+      assert {input, expected} == {input, got}
+    end)
+  end
+
+  test "part2 solution with actual input" do
+    got =
+      File.read!("./data/day1.txt")
+      |> Aoc17.Day1.part2()
+
+    expected = 1284
+    assert expected == got
+  end
 end
