e9b914ad39edc1a1fa55f98cdd255733be86d59c
divxvid <divyansh.shrivastava@students.iiit.ac.in>
2024-04-14 20:51:01 +0530

    Refactor Day1

--- lib/Day1/sol.ex
+++ lib/Day1/sol.ex
@@ -3,16 +3,10 @@ defmodule Aoc17.Day1 do
   def part1(input) do
     input = String.trim(input)
     first_ch = String.at(input, 0)
-    input = input <> first_ch
-    input = String.to_charlist(input)
 
-    Enum.zip(input, Enum.drop(input, 1))
-    |> Enum.reduce(0, fn {last, curr}, acc ->
-      cond do
-        last == curr -> acc + (last - ?0)
-        true -> acc
-      end
-    end)
+    (input <> first_ch)
+    |> String.to_charlist()
+    |> compute_matching_pairs(1)
   end
 
   @spec part2(String.t()) :: integer
@@ -21,9 +15,15 @@ defmodule Aoc17.Day1 do
     target_length = String.length(input) |> div(2)
 
     {left, _} = String.split_at(input, target_length)
-    input = (input <> left) |> String.to_charlist()
 
-    Enum.zip(input, Enum.drop(input, target_length))
+    (input <> left)
+    |> String.to_charlist()
+    |> compute_matching_pairs(target_length)
+  end
+
+  @spec compute_matching_pairs(charlist(), integer) :: integer
+  defp compute_matching_pairs(input, distance) do
+    Enum.zip(input, Enum.drop(input, distance))
     |> Enum.reduce(0, fn {last, curr}, acc ->
       cond do
         last == curr -> acc + (last - ?0)
